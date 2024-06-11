4c16d3daef17e504d7885fb5bbca89850fcde7ab
Tony <tony@weibyapps.com>
2024-06-11 17:26:27 +0800

    chore: misc useless

---
+++ aws-EventBridge/2.main.tf
@@ -0,0 +1,4 @@
+# ==================================================================
+# 
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_event_rule.html
+# ==================================================================
